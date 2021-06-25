22c328706c13f3790fcb328ef251e97eb420e35e
allynsweet <allyn.sweet@uconn.edu>
2021-06-25 14:52:49 -0400

    add unit tests for all functions

---
+++ tests/api.test.ts
@@ -0,0 +1,45 @@
+import IntegrationMailer, { EmailResponse } from '../src/index';
+
+const mailer = new IntegrationMailer();
+let emailList: Array<EmailResponse> = [];
+let emailAddress: string | undefined = '';
+test('should generate an email properly', async () => {
+    emailAddress = await mailer.createEmailAddress();
+    expect(emailAddress).toBeDefined();
+    if (!emailAddress) { return; }
+    expect(emailAddress.includes('@')).toBeTruthy();
+});
+
+test('should return an email list with one email', async () => {
+    emailList = await mailer.fetchEmailList();
+    expect(emailList.length).toEqual(1);
+});
+
+test('should fetch email by ID', async () => {
+    const fullEmail = await mailer.fetchEmailById(emailList[0].mail_id);
+    expect(fullEmail).toBeDefined();
+    if (!fullEmail) { return; }
+    expect(fullEmail.mail_body.length).toBeGreaterThan(250);
+});
+
+test('should wait for email', async () => {
+    const emailResponse: EmailResponse | undefined = await mailer.waitForEmail(emailList[0].mail_subject);
+    expect(emailResponse).toBeDefined();
+    if (!emailResponse) { return; }
+    expect(emailResponse.mail_subject).toEqual(emailList[0].mail_subject);
+});
+
+test('should delete email by ID', async () => {
+    const isEmailDeleted = await mailer.deleteEmailById(emailList[0].mail_id);
+    expect(isEmailDeleted).toBeTruthy();
+    if (!isEmailDeleted) { return; }
+    const secondEmailList = await mailer.fetchEmailList();
+    expect(secondEmailList.length).toEqual(0);
+});
+
+test('should forget email address', async () => {
+    expect(emailAddress).toBeDefined();
+    if (!emailAddress) { return; }
+    const isEmailForgotten = await mailer.forgetEmailAddress(emailAddress);
+    expect(isEmailForgotten).toBeTruthy();
+});

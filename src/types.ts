168d44471abd35699d330adf8d2799f11f7dac26
allynsweet <allyn.sweet@uconn.edu>
2021-08-01 01:38:02 -0400

    centralize shared types

---
+++ src/types.ts
@@ -0,0 +1,28 @@
+export interface CreateEmailResponse {
+    email_addr: string;
+    email_timestamp: number;
+    alias: string;
+    sid_token: string;
+}
+
+export interface EmailListResponse {
+    list: EmailResponse[];
+}
+
+export interface EmailResponse {
+    mail_id: string;
+    mail_from: string;
+    mail_timestamp: string;
+    mail_subject: string;
+    mail_excerpt: string;
+    mail_body: string;
+}
+
+export interface SetEmailResponse {
+    email_addr: string;
+    email_timestamp: string;
+    s_active: string;
+    s_date: string;
+    s_time: string;
+    s_time_expires: string;
+}
